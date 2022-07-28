65e54739945f91f5589b288aa134f46ed80e2231
jsw6701 <anes53027@gmail.com>
2022-07-28 16:58:29 +0900

    강의 진행중 07/28

---
+++ src/test/java/mess/hellospring/service/MemberServiceTest.java
@@ -0,0 +1,33 @@
+package mess.hellospring.service;
+
+import mess.hellospring.domain.Member;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MemberServiceTest {
+
+    MemberService memberService = new MemberService();
+
+    @Test
+    void 회원가입() {
+        //given
+        Member member = new Member();
+        member.setName("hello");
+        //when
+        Long saveId = memberService.join(member);
+        //then
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    void 회원찾기() {
+
+    }
+
+    @Test
+    void findOne() {
+
+    }
+}
